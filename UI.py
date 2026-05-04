7afd53fe7488139e5965d26b23f6f61369342d41
n3fw <quentin.perrigault1303@gmail.com>
2026-05-04 19:51:40 +0200

    added window for connexion

--- UI.py
+++ UI.py
@@ -1,13 +1,15 @@
-import tkinter
+import tkinter as tk
 from PIL import ImageTk
 
 
 class UI():
     def __init__(self):
         self.root = None
-        self.size = "800x600"
+        self.size = "1200x1000"
         self.color = "000000"
-        self.title = "PlaceHolder"
         self.action_id = None
         self.icon = "ressources/main_icon.ico"
         self.progress = None
+    
+    def connexionWindow(self):
+        self.root = tk.Tk()
